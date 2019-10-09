d1ab154d6d37ab5736bf7616aa98d4cca9d7e128
Isaac Winoto <51734988+isaacedwin@users.noreply.github.com>
2019-10-09 11:24:37 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,226 @@
+<html>
+<html lang="en">
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Cashless Canteen</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link href="https://fonts.googleapis.com/css?family=Raleway:400,700&amp;display=swap" rel="stylesheet" />
+	<link href="css/animate.css" rel="stylesheet" />
+	<link href="css/owl.carousel.min.css" rel="stylesheet" />
+	<link href="css/jquery.fancybox.min.css" rel="stylesheet" />
+	<link href="fonts/ionicons/css/ionicons.min.css" rel="stylesheet" />
+	<link href="fonts/fontawesome/css/font-awesome.min.css" rel="stylesheet" />
+	<link href="fonts/flaticon/font/flaticon.css" rel="stylesheet" />
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css" rel="stylesheet" /><!-- Theme Style -->
+	<link href="css/style.css" rel="stylesheet" />
+</head>
+<body>
+<header role="banner">
+<nav class="navbar navbar-expand-lg  bg-dark">
+<div class="container-fluid"><a class="navbar-brand " href="index.html">Cashless Canteen</a><button aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#navbarsExample05" data-toggle="collapse" type="button"></button>
+<div class="collapse navbar-collapse" id="navbarsExample05">
+            <ul class="navbar-nav pl-md-5 ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.html">Our Project</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact Us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="login.php">Login</a>
+              </li>
+            </ul>
+  
+
+
+        </div>
+</div>
+</nav>
+</header>
+<!-- END header -->
+
+<div class="inner-page">
+<div class="slider-item overlay" data-stellar-background-ratio="0.5" style="background-image: url('images/IMG_1545.jpg');">
+<div class="container">
+<div class="row slider-text align-items-center justify-content-center">
+<div class="col-lg-9 text-center col-sm-12 element-animate">
+<h1 class="mb-4">Login</h1>
+<p class="custom-breadcrumbs"><a href="index.html">Home</a> <span class="mx-3">/</span> About</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="section">
+    <center>
+ <form name="login" method="POST">
+     <caption><h1>Login</h1></caption>
+        <table>
+            <tr>
+                <div class="col-md-6 form-group">
+                <td><input type="text" id="username" class="form-control" name="username" placeholder = "Username..."></td>
+            </tr>
+           <p></p>
+             <tr>
+                 <div class="col-md-6 form-group"><label for="password"></label>
+                <td><input type="password" id="password" class="form-control" name="password" placeholder = "Password..."></td>
+            </tr>
+            
+            <tr>
+               <td><center><input type="submit" class="btn btn-primary px-3 py-3" name="login_button"value="Login"></center></td>
+            </tr>
+        </table>
+    </form>
+</center>
+<div class="container">
+<div class="row">
+<!--<div class="col-lg-5 mr-auto mb-5"><span class="d-block text-uppercase text-secondary">Login Page </span>-->
+
+
+
+</div>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+</div>
+</div>
+</div>
+
+
+
+
+<!-- END section -->
+
+<footer class="site-footer" role="contentinfo">
+<div class="container">
+<div class="row mb-5">
+<div class="col-md-4 mb-5">
+<h3 class="mb-4">About Cashless Canteen</h3>
+
+<p class="mb-5">We Are A Computer Science Club That Aims to Provide Digital Solutions to Real-World Problems That We Face Everyday, Together.</p>
+
+<ul class="list-unstyled footer-link d-flex footer-social">
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+</ul>
+</div>
+
+<div class="col-md-5 mb-5 pl-md-5">
+<div class="mb-5">
+<h3 class="mb-4">Contact Info</h3>
+
+<ul class="list-unstyled footer-link">
+	<li class="d-block"><span class="d-block">Address:</span> <span class="text-white">Komplek Taman Meruya Ilir Jalan Batu Mulia Blok K, RT.11/RW.7, Meruya Utara, Kec. Kembangan, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11620</span></li>
+	<li class="d-block"><span class="d-block">Telephone:</span><span class="text-white">+62158905890</span></li>
+	<li class="d-block"><span class="d-block">Email:</span><span class="text-white">isaacedwin777@gmail.com</span></li>
+</ul>
+</div>
+</div>
+
+<div class="col-md-3 mb-5">
+<h3 class="mb-4">Quick Links</h3>
+
+<ul class="list-unstyled footer-link">
+	<li><a href="about.html">Our Project</a></li>
+	<li><a href="contact.php">Contact Us</a></li>
+	<li><a href="login.php">Login</a></li>
+	<li><a href="https://www.instagram.com/csclubiics/">Our Instagram</a></li>
+</ul>
+</div>
+
+<div class="col-md-3"></div>
+</div>
+
+<div class="row">
+<div class="col-12 text-md-center text-left">
+<p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->Copyright &copy; <script>document.write(new Date().getFullYear());</script> All Rights Reserved | This template is made with by <a href="https://colorlib.com" target="_blank">Colorlib</a> <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+</div>
+</div>
+</div>
+</footer>
+<!-- END footer --><!-- loader -->
+
+<div class="show fullscreen" id="loader"><svg class="circular" height="48px" width="48px"> <circle class="path-bg" cx="24" cy="24" fill="none" r="22" stroke="#eeeeee" stroke-width="4"></circle> <circle class="path" cx="24" cy="24" fill="none" r="22" stroke="#f4b214" stroke-miterlimit="10" stroke-width="4"></circle></svg></div>
+<script src="js/jquery-3.2.1.min.js"></script><script src="js/jquery-migrate-3.0.1.min.js"></script><script src="js/popper.min.js"></script><script src="js/bootstrap.min.js"></script><script src="js/owl.carousel.min.js"></script><script src="js/jquery.waypoints.min.js"></script><script src="js/jquery.fancybox.min.js"></script><script src="js/jquery.stellar.min.js"></script><script src="js/main.js"></script></body>
+
+    
+<?php
+
+//echo "hello world inii program apaan woi.... sapmpah gila... lu pulang aja gila... kesel";
+//echo "<br> wadupp";
+
+$servername = "cashlesscanteeniics.com";
+$username = "cashless_theeo";
+$password = "machinelearning";
+$database = "cashless_Login";
+
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    echo "Connection failed";
+} 
+
+$sql = "SELECT * FROM Login_credentials";
+$result = mysqli_query($conn, $sql);
+$username = $_POST["username"];
+$password = $_POST["password"];
+$cred_usernames = array();
+$cred_passwords = array();
+$cred_identity = array();
+
+if (mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+        array_push($cred_usernames, $row["user_name"]);
+        array_push($cred_passwords, $row["password"]);
+        array_push($cred_identity, $row["Identity"]);
+    }
+ 
+
+} else {
+    echo "Database Error...";
+}
+
+
+function redirect($url){
+     echo "redirecting...";
+     $link = "<script> location.href= \"".  $url . "\"; </script>";
+     echo $link;
+     exit();
+}
+//Go through array
+if(isset($_REQUEST["login_button"])){
+for($i = 0; $i <= count($cred_usernames) - 1; $i++){
+    if ($username == $cred_usernames[$i]){
+        if($password == $cred_passwords[$i]){
+            if($cred_identity[$i] == "A"){
+            redirect("admin/mainmenu2.html");
+            }else{
+            redirect("vendor/mainmenu.php");   
+            }
+        }
+    }
+}
+
+
+echo "Wrong username or password";
+
+}
+
+?>
+
+</body>
+</html>
